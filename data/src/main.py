31786060d531a3679803de29df75b7fa797e4634
PaPa-jun <pengyufeng2003@outlook.com>
2024-06-16 00:30:18 +0800

    更新爬虫

--- data/src/main.py
+++ data/src/main.py
@@ -1,11 +1,11 @@
 from module import IMDb, DataBase
 
 if __name__ == "__main__":
-    imdb = IMDb()
+    # imdb = IMDb()
     # movies = imdb.fetch_top_250_movies()
     # imdb.to_json(movies, '/Users/pengyufeng/Desktop/UstcMDb/data/movies/imdb_top_250_movies.json')
     
-    # db = DataBase(host='localhost', password='Pyf20030317', database='ustcMDb')
-    # connection = db.connect_db()
-    # db.load_json_into_db('/home/yufengpeng-wsl/UstcMDb/data/movies/imdb_top_250_movies.json', connection)
-    print(imdb.search_person('Benedict Cumberbatch'))
+    db = DataBase(host='localhost', password='Pyf20030317', database='ustcMDb')
+    connection = db.connect_db()
+    db.load_json_into_db('/Users/pengyufeng/Desktop/UstcMDb/data/movies/imdb_top_250_movies.json', connection)
+    # print(imdb.search_person('Benedict Cumberbatch'))
