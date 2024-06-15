94eb69f895364366266ad896c12852287d392454
zhang123 <zhangzhe777@mail.ustc.edu.cn>
2024-06-15 19:47:27 +0800

    html更新

--- apps/movie/view.py
+++ apps/movie/view.py
@@ -5,3 +5,11 @@ blueprint = Blueprint("movie", __name__)
 @blueprint.route("/top25")
 def top25():
     return "top 25 movies."
+
+@blueprint.route("/recent25")
+def recent25():
+    return "最新25"
+
+@blueprint.route("/classification")
+def classification():
+    return "电影分类"
