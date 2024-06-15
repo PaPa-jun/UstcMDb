7e44b595560e85dc01327a1c81cd457ac0162b2e
PaPa-jun <pengyufeng2003@outlook.com>
2024-06-15 21:00:49 +0800

    同步

--- apps/movie/view.py
+++ apps/movie/view.py
@@ -11,3 +11,18 @@ def top25():
     print(movies)
 
     return render_template("movie/top.html", movies_info = movies)
+
+@blueprint.route("/<id>")
+def movie_detail(id):
+    movie = Movie()
+    current_movie = movie.get_info(g.db, id)
+
+    return render_template("movie/detail.html", movie = current_movie)
+
+@blueprint.route("/recent25")
+def recent25():
+    return "最近25部电影"
+
+@blueprint.route("/classification")
+def classification():
+    return "电影分类"
