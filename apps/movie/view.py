a60fa9d11659654f89810cad1caebe32ce35fb28
zhang123 <zhangzhe777@mail.ustc.edu.cn>
2024-06-15 21:49:09 +0800

    更新top25

--- apps/movie/view.py
+++ apps/movie/view.py
@@ -8,8 +8,6 @@ def top25():
     movie = Movie()
     movies = movie.top(g.db, 25)
 
-    print(movies)
-
     return render_template("movie/top.html", movies_info = movies)
 
 @blueprint.route("/<id>")
